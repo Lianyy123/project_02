55192c064954d0d8be5f948a4e836e1b6d29da46
lianyangyang <2543645827@qq.com>
2022-02-24 09:11:51 +0800

    新提交的文件

--- login.js
+++ login.js
@@ -1,21 +1,35 @@
 var ac = document.querySelector('.input');
 var password = document.querySelector('.password_input');
 var dengLu = document.getElementById('a');
-var bottom_tiShi = document.querySelector('.bottom_tiShi');
+var bottom_tiShiError = document.querySelector('.bottom_tiShiError');
+var bottom_tiShiTip = document.querySelector('.bottom_tiShiTip');
    dengLu.onclick = function(){
   if(ac.value == '1' && password.value == "1"){
       alert('登陆成功！');
   }
    else if (ac.value == '' && password.value == ""){
-       alert('请输入账号和密码后点击登录');
-   }
-   else {
-    bottom_tiShi.style.display = 'block';
+    bottom_tiShiTip.style.display = 'block';
     ac.onclick = function(){
-        bottom_tiShi.style.display = 'none';
+        bottom_tiShiTip.style.display = 'none';
     };
     password.onclick = function(){
-        bottom_tiShi.style.display = 'none';
+        bottom_tiShiTip.style.display = 'none';
     };
    }
+   else {
+    bottom_tiShiError.style.display = 'block';
+    ac.onclick = function(){
+        bottom_tiShiError.style.display = 'none';
+    };
+    password.onclick = function(){
+        bottom_tiShiError.style.display = 'none';
+    };
+   }
+};
+
+//点击右上角关闭登录页面
+var dengLu_yeMian = document.getElementById('id_dengLu_yeMian');
+var delect = document.getElementById('delect');
+delect.onclick = function(){
+    dengLu_yeMian.style.display ='none';
 };
